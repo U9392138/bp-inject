5d8d0e7421c44571ed5fa6b984020f62dd90fa88
Peter Norton <sala2801@hotmail.com>
2025-06-11 13:42:08 -0300

    inital up
    adiciona arquivos js para uso com redirecionamento

---
+++ app.js
@@ -0,0 +1,37 @@
+import { record } from "./vmsg.js";
+
+let recordButton = document.getElementById("buttonMensagemVoz");
+recordButton.onclick = function() {
+  record({wasmURL: "vmsg.wasm"}).then(blob => {
+
+
+    var formData = new FormData();
+    formData.append('file', blob);
+
+    $.ajax({
+       url: "AcoesUnificadas.php?AcoesTipo=BPP&Acao=enviarMensagemVoz",
+       type: 'POST',
+       data: formData,
+       contentType: false,
+       processData: false,
+       dataType: 'json'
+    })
+
+    .done(function(data) {
+
+    })
+
+    .fail(function(data) {
+       var jsonData = data.responseJSON;
+       alert(jsonData.retornoMensagem);
+    });
+
+
+    
+    //console.log("Recorded MP3", blob);
+    //var url = URL.createObjectURL(blob);
+    //var preview = document.createElement('audio');
+    //preview.controls = true;preview.src = url;
+    //document.body.appendChild(preview);
+  });
+};
